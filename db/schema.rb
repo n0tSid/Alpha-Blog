11848b6684d8e8a46e8958306669448f9f695e93
sabrina214 <siddhantroy214@protonmail.com>
2022-01-29 19:20:28 +0530

    Added validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_085225) do
+ActiveRecord::Schema.define(version: 2022_01_29_085908) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2022_01_29_085225) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
